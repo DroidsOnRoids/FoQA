a4327718932e2778c211c94d09e251830eec4188
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2019-02-27 02:24:44 +0100

    Extract device info plugin to separate module Add sample app

---
+++ device_info_plugin/src/main/java/pl/droidsonroids/foqa/deviceinfo/DeviceInfoPlugin.kt
@@ -0,0 +1,30 @@
+package pl.droidsonroids.foqa.deviceinfo
+
+import android.annotation.SuppressLint
+import android.os.Build
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.google.auto.service.AutoService
+import com.jaredrummler.android.device.DeviceName
+import com.willowtreeapps.hyperion.plugin.v1.Plugin
+import com.willowtreeapps.hyperion.plugin.v1.PluginModule
+
+@AutoService(Plugin::class)
+class DeviceInfoPlugin : Plugin() {
+    override fun createPluginModule() = DeviceInfoModule()
+}
+
+class DeviceInfoModule : PluginModule() {
+
+    @SuppressLint("SetTextI18n")
+    override fun createPluginView(layoutInflater: LayoutInflater, parent: ViewGroup): View {
+        val view = layoutInflater.inflate(R.layout.menu_item, parent, false) as TextView
+        val manufacturer = Build.MANUFACTURER
+        val deviceModel = DeviceName.getDeviceName()
+        val sdk = Build.VERSION.RELEASE
+        view.text = "$manufacturer $deviceModel Android: $sdk"
+        return view
+    }
+}

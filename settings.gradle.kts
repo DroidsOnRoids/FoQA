82a90527f8d8c5d71e7d6c6958cdee63a3d98930
Karol Wrótniak <karol.wrotniak@droidsonroids.pl>
2021-08-25 20:38:33 +0200

    Remove hardcoded version Agree to Gradle terms of service

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,3 +3,10 @@ plugins {
 }
 
 include(":foqa", ":device_info_plugin", ":font_scale_plugin", ":chucker_plugin", ":sample")
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}

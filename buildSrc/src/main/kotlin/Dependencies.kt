d1a7fae6f1efc1e349fd9e72ea45d1eb7a0031af
Artur Kosta <artur.kosta@droidsonroid.pl>
2022-08-02 10:18:53 +0200

    Bump versions

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -3,10 +3,10 @@ object Dependencies {
     const val targetSdk = 31
     const val minSdk = 19
 
-    const val autoService = "com.google.auto.service:auto-service:1.0"
-    const val androidxAnnotations = "androidx.annotation:annotation:1.3.0-beta01"
-    const val detektFormatting = "io.gitlab.arturbosch.detekt:detekt-formatting:1.18.1"
-    const val detektCli = "io.gitlab.arturbosch.detekt:detekt-cli:1.18.1"
+    const val autoService = "com.google.auto.service:auto-service:1.0.1"
+    const val androidxAnnotations = "androidx.annotation:annotation:1.4.0"
+    const val detektFormatting = "io.gitlab.arturbosch.detekt:detekt-formatting:1.21.0"
+    const val detektCli = "io.gitlab.arturbosch.detekt:detekt-cli:1.21.0"
     const val hyperionCore = "com.willowtreeapps.hyperion:hyperion-core:0.9.34"
     const val hyperionPlugin = "com.willowtreeapps.hyperion:hyperion-plugin:0.9.34"
     const val hyperionAttr = "com.willowtreeapps.hyperion:hyperion-attr:0.9.34"
@@ -22,12 +22,12 @@ object Dependencies {
     const val hyperionBuildConfig = "com.willowtreeapps.hyperion:hyperion-build-config:0.9.34"
     const val hyperionAppInfo = "com.star-zero:hyperion-appinfo:2.0.0"
     const val chucker = "com.github.ChuckerTeam.Chucker:library:3.5.2"
-    const val deviceNames = "com.jaredrummler:android-device-names:2.0.0"
+    const val deviceNames = "com.jaredrummler:android-device-names:2.1.0"
     const val debugDb = "com.github.amitshekhariitbhu.Android-Debug-Database:debug-db:1.0.6"
     const val multidex = "androidx.multidex:multidex:2.0.1"
     const val androidGradlePlugin = "com.android.tools.build:gradle:7.2.1"
     const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:1.5.31"
-    const val publishGradlePlugin = "com.vanniktech:gradle-maven-publish-plugin:0.18.0"
-    const val okHttpBom = "com.squareup.okhttp3:okhttp-bom:4.9.2"
+    const val publishGradlePlugin = "com.vanniktech:gradle-maven-publish-plugin:0.21.0"
+    const val okHttpBom = "com.squareup.okhttp3:okhttp-bom:4.10.0"
     const val okHttp = "com.squareup.okhttp3:okhttp"
 }
